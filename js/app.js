8a2758b3e52dac3aeea25e0f648ad596669d9bba
brunEdo <edobruno.m@gmail.com>
2021-09-06 17:47:45 +0200

    js refactoring

--- js/app.js
+++ js/app.js
@@ -3,54 +3,63 @@
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+let level = 0;
+let levelNumbers = level + 5;
+
 // 1. generate random number
 const randomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-let number1 = randomNumber(0, 99);
-let number2 = randomNumber(0, 99);
-let number3 = randomNumber(0, 99);
-let number4 = randomNumber(0, 99);
-let number5 = randomNumber(0, 99);
+let numbers = []
+for (let i = 0; i < levelNumbers; i++) {
+    numbers.push(randomNumber(0, 99));
+}
 
 // 2. alert 5 random number
 alert("remember these numbers");
-alert(`${number1} - ${number2} - ${number3} - ${number4} - ${number5}`);
+alert(numbers);
 
 // 3. set a timer of 30 seconds
 setTimeout( () => {
     // 4. ask (prompt) which number the user remember, one by one
-    let userNumber1 = +prompt("Tell me the 1st number");
-    let userNumber2 = +prompt("Tell me the 2nd number");
-    let userNumber3 = +prompt("Tell me the 3rd number");
-    let userNumber4 = +prompt("Tell me the 4th number");
-    let userNumber5 = +prompt("Tell me the 5th number");
-    // 5. comunicate the result of the test to the user
-    let noMistakes = true;
-    if ( number1 !== userNumber1 ) { 
-        alert("The 1st number is wrong");
-        noMistakes = false;
-    }
-    if ( number2 !== userNumber2 ) { 
-        alert("The 2nd number is wrong");
-        noMistakes = false; 
-    }
-    if ( number3 !== userNumber3 ) { 
-        alert("The 3rd number is wrong");
-        noMistakes = false;
-    }
-    if ( number4 !== userNumber4 ) { 
-        alert("The 4th number is wrong");
-        noMistakes = false; 
-    }
-    if ( number5 !== userNumber5 ) { 
-        alert("The 5th number is wrong");
-        noMistakes = false; 
+    let userNumbers = []
+    for (let i = 0; i < levelNumbers; i++) {
+        switch (i) {
+            case 0: 
+                numbers.push(+prompt("Tell me the 1st number"));
+                break;
+            case 1: 
+                numbers.push(+prompt("Tell me the 2nd number"));
+                break;
+            case 2: 
+                numbers.push(+prompt("Tell me the 3rd number"));
+                break;
+            default: 
+                numbers.push(+prompt(`Tell me the ${i + 1}th number`));
+        }
     }
 
-    if ( noMistakes ) {
+    // 5. comunicate the result of the test to the user
+    for (let i = 0; i < levelNumbers; i++) {
+        switch (i) {
+            case 0: 
+                alert("The 1st number is wrong");
+                break;
+            case 1: 
+                alert("The 2nd number is wrong");
+                break;
+            case 2: 
+                alert("The 3rd number is wrong");
+                break;
+            default: 
+                alert(`The ${i + 1}th number is wrong`);
+        }
+    }
+
+    if ( numbers === userNumbers ) {
         alert("Well done!!!");
+        level++;
     } else {
         alert("Refresh and try again")
     }
-}, 30000)
+}, 30000);
