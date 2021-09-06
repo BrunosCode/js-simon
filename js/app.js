e107364ea10137136a52ede4c38ae368041e7223
brunEdo <edobruno.m@gmail.com>
2021-09-06 21:49:58 +0200

    js refactoring + restart game

--- js/app.js
+++ js/app.js
@@ -3,67 +3,161 @@
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-let level = 0;
-let levelNumbers = level + 1;
 
+// 0. SHORTCUT VARIABLES
+const startBtn = document.getElementById("start-btn");
+
+const instructionRow = document.getElementById("instruction-row");
+const countDown = document.getElementById("count-down");
+
+const numbersRow = document.getElementById("numbers-row");
+
+const inputsRow = document.getElementById("inputs-row");
+
+const submitBtn = document.getElementById("submit-btn");
+
+const results = document.getElementById("results-col");
+
+const restartBtn = document.getElementById("restart-btn");
+
+// FUNCTIONS
 // 1. generate random number
 const randomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-let numbers = []
-for (let i = 0; i < levelNumbers; i++) {
-    numbers.push(randomNumber(0, 99));
+
+const show = (element) => {
+    element.classList.remove("hide");
+} 
+const hide = (element) => {
+    element.classList.add("hide");
+} 
+
+// GAMES VARIABLE
+let level = 5;
+let numbers = [];
+let userNumbers = [];
+
+// GAME SCRIPT: FIRST PART
+const startGame = () => {
+
+    numbers = [];
+    for (let i = 0; i < level; i++) {
+        let number = randomNumber(0, 9);
+        numbers.push(number);
+        numbersRow.innerHTML += `<div>${number}</div>`;
+    }
+    console.log(numbers)
+    
+    // Hide startBtn 
+    hide(startBtn);
+    // Show numbersRow
+    show(numbersRow);
+
+    // Set Istructions
+    instructionRow.innerHTML = `<h1>Remember these numbers in <span id="count-down">30</span> seconds</h1>`;
+    
+    // 3. set a timer of 30 seconds
+    setTimeout( () => {
+    
+        // Hide instrucionRow and numberRow
+        hide(numbersRow);
+        // Show inputsRow and submitBtn
+        show(inputsRow);
+        show(submitBtn);
+    
+        // Set Istructions
+        instructionRow.innerHTML = `<h1>Enter the numbers you remember</h1>`;
+
+        // clean inputs row
+        inputsRow.innerHTML = "";
+
+        // 4. Generate html inputs
+        for (let i = 0; i < level; i++) {    
+            inputsRow.innerHTML += `<input type="text" name="number">`;
+        }
+    
+    }, 3000);
 }
 
-// 2. alert 5 random number
-alert("remember these numbers");
-alert(numbers);
+// GAME SCRIPT: SECOND PART
+const endGame = () => {
 
-// 3. set a timer of 30 seconds
-setTimeout( () => {
-    // 4. ask (prompt) which number the user remember, one by one
-    let userNumbers = []
-    for (let i = 0; i < levelNumbers; i++) {
-        switch (i) {
-            case 0: 
-                userNumbers.push(+prompt("Tell me the 1st number"));
-                break;
-            case 1: 
-                userNumbers.push(+prompt("Tell me the 2nd number"));
-                break;
-            case 2: 
-                userNumbers.push(+prompt("Tell me the 3rd number"));
-                break;
-            default: 
-                userNumbers.push(+prompt(`Tell me the ${i + 1}th number`));
-        }
+    // get user inputs
+    userNumbers = [];
+    for (let i = 0; i < level; i++) {
+        console.log(+inputsRow.childNodes[i].value);
+        userNumbers.push(+inputsRow.childNodes[i].value);
     }
+    
+    // Hide inputsRow and submitRow
+    hide(inputsRow);
+    hide(submitBtn);
+    hide(instructionRow)
 
-    // 5. comunicate the result of the test to the user
+    // Show results
+    show(results);
+    show(restartBtn);
+    
+    // Set Istructions
+    instructionRow.innerHTML = "";
+
+    // 5. comunicate each error to the user
     let noMistakes = true;
-    for (let i = 0; i < levelNumbers; i++) {
+    for (let i = 0; i < level; i++) {
         if ( userNumbers[i] !== numbers[i] ) {
             noMistakes = false;
             switch (i) {
                 case 0: 
-                    alert("The 1st number is wrong");
+                    results.innerHTML += "<div>The 1st number is wrong</div>";
                     break;
                 case 1: 
-                    alert("The 2nd number is wrong");
+                    results.innerHTML += "<div>The 2nd number is wrong</div>";
                     break;
                 case 2: 
-                    alert("The 3rd number is wrong");
+                    results.innerHTML += "<div>The 3rd number is wrong</div>";
                     break;
                 default: 
-                    alert(`The ${i + 1}th number is wrong`);
+                    results.innerHTML += `<div>The ${i + 1}th number is wrong</div>`;
             }
         }
     }
-
+    
+    // Show the result
     if ( noMistakes ) {
-        alert("Well done!!!");
+        results.innerHTML = "<h2>Well done!!!</h2>" + results.innerHTML;
+
+        // If user wins the game add 1 level
         level++;
     } else {
-        alert("Refresh and try again")
+        results.innerHTML = "<h2>Try again</h2>" + results.innerHTML;
     }
-}, 30000);
+}
+
+// START BUTTON
+startBtn.addEventListener( "click", startGame );
+
+
+// SUBMIT BUTTON
+submitBtn.addEventListener( "click", endGame );
+
+// RESTART BUTTON
+restartBtn.addEventListener( "click", () => {
+
+    // Hide results
+    hide(results);
+    hide(restartBtn);
+    // Show startCol and instructionRow
+    show(instructionRow);
+    show(startBtn);
+
+    // Set Istructions
+    instructionRow.innerHTML = `<h1>Click to play</h1>`;
+
+    // Remove injected html
+    numbersRow.innerHTML = "";
+    inputsRow.innerHTML = "";
+    results.innerHTML = "";
+
+    startGame();
+});
